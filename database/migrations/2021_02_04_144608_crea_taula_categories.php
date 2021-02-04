c6d4a3d4d4e547d910b46de0da4c6e9a7fb94d80
Enric <emieza@xtec.cat>
2021-02-04 16:12:31 +0100

    created categories table

---
+++ database/migrations/2021_02_04_144608_crea_taula_categories.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreaTaulaCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('parent');
+            $table->foreign('parent')->references('id')->on('categories');
+            $table->timestamps();
+        });
+
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->unsignedBigInteger('cat_id');
+            $table->foreign('cat_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}

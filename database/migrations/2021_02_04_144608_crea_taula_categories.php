2c6ae5f6c071b87d37bb162280976ca0f94c625e
silviadlc <57746795+silviadlc@users.noreply.github.com>
2021-02-04 16:53:49 +0100

    created categories

--- database/migrations/2021_02_04_144608_crea_taula_categories.php
+++ database/migrations/2021_02_04_144608_crea_taula_categories.php
@@ -16,7 +16,7 @@ class CreaTaulaCategories extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedBigInteger('parent');
+            $table->unsignedBigInteger('parent')->nullable();
             $table->foreign('parent')->references('id')->on('categories');
             $table->timestamps();
         });
